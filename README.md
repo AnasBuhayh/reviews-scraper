cbce3e9b86d782e949360bfe155a5b68adad86cf
anas bhih <bhih.anas@gmail.com>
2020-12-10 18:37:40 -0500

    add product input feature

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # reviews-scrapper
 Walmart online reviews scrapper.
-All you have to do to get the reviews is to type the product id and you'll get all the reviews for that product.
-
+All you have to do to get the reviews is to run the script and then enter the product id which is the last part of the product page
 ## Disclaimer
 Web scrapping is still a gray area in the tech world. please use this at your own responsibility.
 
 ## Future features
-- modify the script to run from the commandline
 - adding validations and exceptions
--- reviews_scrapper.py
+++ reviews_scrapper.py
@@ -1,7 +1,7 @@
 from requests_html import HTMLSession
-import re
 import pandas as pd
 import time
+import re
 
 df = pd.DataFrame(columns = ['Date', 'Title', 'Review_Text', 'Product', 'Rating'])
 dates = []
@@ -9,49 +9,60 @@ titles = []
 texts = []
 ratings = []
 
-#Type product id
-product = '' 
+def get_reviews(link):
+    session = HTMLSession()
+    r = session.get(link)
+    num_pages = int(r.html.find('.paginator-list', first=True).text.split('\n')[-1])
+    product_name = r.html.find('.prod-ProductTitle', first=True).text
+    file_name = product_name.translate({ord(c): "_" for c in "\"!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})
 
-link = f'https://www.walmart.com/reviews/product/{product}'
+    for page in range(num_pages):
+        page_link = link + '?page=' + str(page)
+        r = session.get(page_link)
 
-session = HTMLSession()
-r = session.get(link)
-num_pages = int(r.html.find('.paginator-list', first=True).text.split('\n')[-1])
-product_name = r.html.find('.prod-ProductTitle', first=True).text
-file_name = product_name.translate({ord(c): "_" for c in "\"!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})
+        reviews = r.html.find('.review')
+        for review in reviews:
+            date = review.find('.review-date-submissionTime', first=True).text 
+            try:
+                title = review.find('.review-title', first=True).text
+            except:
+                title = "none"
+            try:
+                text = review.find('.review-text', first=True).text 
+            except:
+                text = "none"
+            rating = review.find('.seo-avg-rating', first=True).text 
 
-for page in range(num_pages):
-    page_link = link + '?page=' + str(page)
-    r = session.get(page_link)
+            dates.append(date)
+            titles.append(title)
+            texts.append(text)
+            ratings.append(rating)
 
-    reviews = r.html.find('.review')
-    for review in reviews:
-        date = review.find('.review-date-submissionTime', first=True).text 
-        try:
-            title = review.find('.review-title', first=True).text
-        except:
-            title = "none"
-        try:
-            text = review.find('.review-text', first=True).text 
-        except:
-            text = "none"
-        rating = review.find('.seo-avg-rating', first=True).text 
+        print(f'scrapping page: {page + 1} out of {num_pages}')
+        time.sleep(0.1)
+        
+    df['Rating'] = ratings
+    df['Title'] = titles
+    df['Date'] = dates
+    df['Review_Text'] = texts
+    df['Product'] = product_name
 
-        dates.append(date)
-        titles.append(title)
-        texts.append(text)
-        ratings.append(rating)
+    df.to_csv(index=False, path_or_buf=f'{product}.csv')
 
-    print(f'scrapping page: {page + 1} out of {num_pages}')
-    time.sleep(0.1)
-    
-df['Rating'] = ratings
-df['Title'] = titles
-df['Date'] = dates
-df['Review_Text'] = texts
-df['Product'] = product_name
+    print('Done Scrapping!')
 
-df.to_csv(index=False, path_or_buf=f'{file_name}.csv')
+
+product = input("Enter Product Id: ")
+
+try:
+   product = int(product)
+   if type(product) == int:
+       link = f'https://www.walmart.com/reviews/product/{product}'
+       get_reviews(link)
+except ValueError:
+   print("Unvalid Input!")
+
+
 
 
     
